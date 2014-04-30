96284ebcf52f16d4f0c6cf076aa1e66f2b930691
Jure Zbontar <jure.zbontar@gmail.com>
2014-04-30 19:20:09 +0200

    No random bugs. It just works!!!1

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ require 'torch'
 require 'cunn'
 require 'cutorch'
 require 'nn'
+require 'jzt'
 require 'prof-torch'
 
 cutorch.setDevice(arg[1])
@@ -10,39 +11,29 @@ cutorch.setDevice(arg[1])
 torch.manualSeed(42)
 
 batch_size = 128
+wc = 0.001
 momentum = 0.9
-weight_decay = 0
 learning_rate = 0.001
-learning_rate_decay = 1
 
-data_path = '/misc/vlgscratch2/LecunGroup/goroshin/Data/data_gcn_whitened/'
-X = torch.FloatTensor(torch.FloatStorage(data_path .. 'X')):resize(60000, 3, 32, 32)
-y = torch.FloatTensor(torch.FloatStorage(data_path .. 'y'))
+data_path = 'data_gcn_whitened/'
+X = torch.FloatTensor(torch.FloatStorage(data_path .. 'X')):resize(60000, 3, 32, 32):cuda()
+y = torch.FloatTensor(torch.FloatStorage(data_path .. 'y')):cuda()
 
-X = X:cuda()
-y = y:cuda()
-
-pool = nn.SpatialLPPooling(64, 2, 2, 2, 2, 2)
-pool:get(3).eps = 1e-6
-
-net = nn.Sequential{bprop_min=1,debug=0}
+net = nn.Sequential{bprop_min=2,debug=0}
 net:add(nn.SpatialZeroPadding(2, 2, 2, 2))
 net:add(nn.SpatialConvolutionRing2(3, 64, 5, 5))
-net:add(nn.Threshold())
---net:add(nn.SpatialMaxPooling(2, 2, 2, 2))
-net:add(pool:clone())
+net:add(jzt.Relu())
+net:add(jzt.L2Pooling(2, 2))
 
 net:add(nn.SpatialZeroPadding(2, 2, 2, 2))
 net:add(nn.SpatialConvolutionRing2(64, 64, 5, 5))
-net:add(nn.Threshold())
---net:add(nn.SpatialMaxPooling(2, 2, 2, 2))
-net:add(pool:clone())
+net:add(jzt.Relu())
+net:add(jzt.L2Pooling(2, 2))
 
 net:add(nn.SpatialZeroPadding(2, 2, 2, 2))
 net:add(nn.SpatialConvolutionRing2(64, 64, 5, 5))
-net:add(nn.Threshold())
---net:add(nn.SpatialMaxPooling(2, 2, 2, 2))
-net:add(pool:clone())
+net:add(jzt.Relu())
+net:add(jzt.L2Pooling(2, 2))
 
 -- print(net:cuda():forward(X:narrow(1, 1, 128)):size())
 
@@ -56,30 +47,46 @@ measure = nn.ClassNLLCriterion()
 net = net:cuda()
 measure = measure:cuda()
 
-parameters, grad_parameters = net:getParameters()
-grad_momentum = torch.CudaTensor(grad_parameters:nElement()):zero()
+layers = {}
+for i = 1,net:size() do
+   if net:get(i).weight then
+      layer = net:get(i)
+      layer.weight_v = torch.CudaTensor():resizeAs(layer.weight):zero()
+      layer.bias_v = torch.CudaTensor():resizeAs(layer.bias):zero()
+      layer.gradWeight:zero()
+      layer.gradBias:zero()
+      layers[#layers + 1] = layer
+   end
+end
 
-start = prof.time()
+t = prof.time()
 for epoch = 1,100 do
+   -- train
    for t = 1,50000 - batch_size,batch_size do
       X_batch = X:narrow(1, t, batch_size)
       y_batch = y:narrow(1, t, batch_size)
       net:forward(X_batch)
       measure:forward(net.output, y_batch)
       measure:backward(net.output, y_batch)
-      net:zeroGradParameters()
       net:backward(X_batch, measure.gradInput)
 
-      grad_momentum:mul(momentum)
-      grad_momentum:add(-weight_decay * learning_rate, parameters)
-      grad_momentum:add(-learning_rate, grad_parameters)
+      -- gradient step
+      for _, layer in ipairs(layers) do
+         layer.weight_v:mul(momentum):add(-learning_rate * wc, layer.weight):add(-learning_rate, layer.gradWeight)
+         layer.weight:add(layer.weight_v)
 
-      parameters:add(grad_momentum)
-      learning_rate = learning_rate * learning_rate_decay
+         layer.bias_v:mul(momentum):add(-learning_rate * wc, layer.bias):add(-learning_rate, layer.gradBias)
+         layer.bias:add(layer.bias_v)
+
+         -- zeroGradParameters
+         layer.gradWeight:zero()
+         layer.gradBias:zero()
+      end
    end
 
+   -- test
    if epoch % 1 == 0 then
-      -- torch.save(('net/%05d.bin'):format(epoch), net)
+      torch.save(('net/%05d.bin'):format(epoch), net)
       err = 0
       for t = 50001,60000 - batch_size,batch_size do
          X_batch = X:narrow(1, t, batch_size)
@@ -88,6 +95,6 @@ for epoch = 1,100 do
          _, i = net.output:float():max(2)
          err = err + i:float():ne(y_batch:float()):sum()
       end
-      print(epoch, err / 10000, prof.time() - start)
+      print(epoch, err / 10000, prof.time() - t)
    end
 end
